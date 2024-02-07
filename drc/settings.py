2dc8e6c6f7b3be7dc8d67c21a95255550cba88f1
nash-dd <nash.lomeli@doordash.com>
2024-02-07 00:36:46 -0800

    remove duplicate dj_database_url

--- drc/settings.py
+++ drc/settings.py
@@ -105,10 +105,6 @@ WSGI_APPLICATION = 'drc.wsgi.application'
 #     }
 # }
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
